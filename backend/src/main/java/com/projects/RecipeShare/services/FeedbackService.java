1d94956ba1f56616474f3c4ab18dde55935ef3f9
LorenzoBondan <lbbondan@gmail.com>
2023-07-09 14:54:48 -0300

    feedbackService

---
+++ backend/src/main/java/com/projects/RecipeShare/services/FeedbackService.java
@@ -0,0 +1,73 @@
+package com.projects.RecipeShare.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projects.RecipeShare.dto.FeedbackDTO;
+import com.projects.RecipeShare.entities.Feedback;
+import com.projects.RecipeShare.repositories.FeedbackRepository;
+import com.projects.RecipeShare.repositories.RecipeRepository;
+import com.projects.RecipeShare.repositories.UserRepository;
+import com.projects.RecipeShare.services.exceptions.DataBaseException;
+import com.projects.RecipeShare.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class FeedbackService{
+
+	@Autowired
+	private FeedbackRepository repository;
+	
+	@Autowired
+	private RecipeRepository recipeRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<FeedbackDTO> findAllPaged(Pageable pageable) {
+		Page<Feedback> list = repository.findAll(pageable);
+		return list.map(x -> new FeedbackDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public FeedbackDTO findById(Long id) {
+		Optional<Feedback> obj = repository.findById(id);
+		Feedback entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+		return new FeedbackDTO(entity);
+	}
+
+	@Transactional
+	public FeedbackDTO insert(FeedbackDTO dto) {
+		Feedback entity = new Feedback();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new FeedbackDTO(entity);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity Violation");
+		}
+	}
+
+	private void copyDtoToEntity(FeedbackDTO dto, Feedback entity) {
+		entity.setComment(dto.getComment());
+		entity.setPontuation(dto.getPontuation());
+		entity.setMoment(dto.getMoment());
+		entity.setRecipe(recipeRepository.getOne(dto.getRecipeId()));
+		entity.setUser(userRepository.getOne(dto.getUser().getId()));
+	}
+}
