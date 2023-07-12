299143bb7cf431bd0dc821c99de931433969039a
LorenzoBondan <lbbondan@gmail.com>
2023-07-12 16:22:35 -0300

    myRecipes and myFavoriteRecipes in profile page

---
+++ frontend/src/pages/Profile/FavoritedRecipeCard/index.tsx
@@ -0,0 +1,139 @@
+import { Recipe, User } from "types";
+import starIcon from 'assets/images/star.png';
+import like from 'assets/images/like.png';
+import likeFilled from 'assets/images/like_filled.png';
+import { useCallback, useEffect, useState } from "react";
+import { requestBackend } from "util/requests";
+import { AxiosRequestConfig } from "axios";
+import { getTokenData } from "util/auth";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+type Props = {
+    recipeId: number;
+    onUpdateFavorite: Function;
+}
+
+const FavoritedRecipeCard = ({recipeId, onUpdateFavorite} : Props) => {
+
+    const [user, setUser] = useState<User | null>(null);
+
+    const getUser = useCallback(async () => {
+      try {
+        const email = getTokenData()?.user_name;
+  
+        if (email) {
+          const params: AxiosRequestConfig = {
+            method: "GET",
+            url: `/users/email/${email}`,
+            withCredentials: true,
+          };
+  
+          const response = await requestBackend(params);
+          setUser(response.data);
+        }
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    }, []);
+  
+    useEffect(() => {
+      getUser();
+    }, [getUser]);
+
+    const [recipe, setRecipe] = useState<Recipe>(); 
+
+    const getRecipe = useCallback(() => {
+        const params : AxiosRequestConfig = {
+          method:"GET",
+          url: `/recipes/${recipeId}`,
+          withCredentials:true
+        }
+        requestBackend(params) 
+          .then(response => {
+            setRecipe(response.data);
+          })
+    }, [recipeId])
+
+    useEffect(() => {
+        getRecipe();
+    }, [getRecipe]);
+
+    const plotStars = (quantity: number) => {
+        const arrayImages = Array.from({length: quantity});
+
+        return(
+            <div style={{display:"flex", flexDirection:"row"}}>
+                {quantity !== 0 ? 
+                    arrayImages.map((_) => (
+                        <img src={starIcon} alt="" style={{height:"20px", marginLeft:"5px"}}/>
+                    )) : 
+                        <p></p>
+                }
+            </div>
+        );
+    };
+
+    const addRecipeAsFavorite = () => {
+        const params : AxiosRequestConfig = {
+            method:"PUT",
+            url: `/recipes/addFavorite/${recipe?.id}`,
+            withCredentials:true
+          }
+          requestBackend(params) 
+            .then(response => {
+                onUpdateFavorite();
+                toast.info(`${recipe?.name} added to your favorites!`, {
+                    className: 'custom-toast',
+                    bodyClassName: 'custom-toast-body',
+                });
+            })
+    }
+
+    const removeRecipeAsFavorite = () => {
+        const params : AxiosRequestConfig = {
+            method:"PUT",
+            url: `/recipes/removeFavorite/${recipe?.id}`,
+            withCredentials:true
+          }
+          requestBackend(params) 
+            .then(response => {
+                onUpdateFavorite();
+                toast.info(`${recipe?.name} removed from your favorites!`, {
+                    className: 'custom-toast',
+                    bodyClassName: 'custom-toast-body-removed',
+                });
+            })
+    }
+    
+    return(
+        <div className="recipe-card-container" style={{backgroundImage: `url(${recipe?.imgUrl})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        }}>
+            <div className="recipe-card-content">
+                <div className="recipe-card-first-container">
+                    <Link to={`/recipes/${recipe?.id}`}>
+                        <div className="recipe-card-title">
+                            <h4>{recipe?.name}</h4>
+                        </div>
+                    </Link>
+                    <div className="recipe-card-buttons">
+                        {user && recipe?.usersFavoritedId.includes(user?.id) ? (
+                            <img src={likeFilled} alt="" onClick={() => removeRecipeAsFavorite()}/>
+                        ) : (
+                            <img src={like} alt="" onClick={() => addRecipeAsFavorite()}/>
+                        )}
+                        <p>{recipe?.usersFavoritedId.length}</p>
+                    </div>
+                </div>
+                <div className="recipe-card-second-container">
+                    {recipe && plotStars(recipe?.pontuationAverage)}
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default FavoritedRecipeCard;
--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -4,6 +4,11 @@ import { User } from 'types';
 import { getTokenData } from 'util/auth';
 import { AxiosRequestConfig } from 'axios';
 import { requestBackend } from 'util/requests';
+import RecipeCard from 'Components/RecipeCard';
+import FavoritedRecipeCard from './FavoritedRecipeCard';
+import { GiNoodles } from 'react-icons/gi';
+import { AiFillStar } from 'react-icons/ai';
+import { BsFillGearFill } from 'react-icons/bs';
 
 const Profile = () => {
 
@@ -35,9 +40,12 @@ const Profile = () => {
     return(
         <div className='profile-container'>
             <div className='profile-user-info base-card'>
-                <img src={user?.imgUrl} alt="" />
+                <div className='profile-image'>
+                    <img src={user?.imgUrl} alt="" />
+                </div>
+                
                 <h2>{user?.name}</h2>
-                <p>{user?.email}</p>
+                <p style={{fontFamily: "Be Vietnam Pro"}}>{user?.email}</p>
                 <div className='profile-user-badge'>
                     <div className='profile-badge-item'>
                         <h5>{user?.recipes.length}</h5>
@@ -55,15 +63,29 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className='profile-user-edit'>
-                    <button className='btn btn-primary'>Edit Profile</button>
+                    <button className='btn btn-primary'><BsFillGearFill style={{marginRight:"3px"}}/> Edit Profile</button>
                 </div>
             </div>
             <div className='profile-user-recipes'>
-                <div className='profile-my-recipes'>
-
+                <div className='profile-my-recipes base-card'>
+                    <h3><GiNoodles style={{marginRight:"5px"}}/> My Recipes <h6>({user?.recipes.length})</h6></h3>
+                    <div className='row my-recipes-row'>
+                        {user?.recipes.map(recipe => (
+                            <div className='col-sm-12 col-md-12 col-lg-6 col-xl-6 recipes-column'>
+                                <RecipeCard recipe={recipe} onUpdateFavorite={getUser} key={recipe.id}/>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className='profile-favorited-recipes'>
-
+                <div className='profile-favorited-recipes base-card'>
+                    <h3><AiFillStar style={{marginRight:"5px"}}/> My Favorited Recipes <h6>({user?.favoritesId.length})</h6></h3>
+                    <div className='row my-recipes-row'>
+                        {user?.favoritesId.map(favoriteId => (
+                            <div className='col-sm-12 col-md-12 col-lg-6 col-xl-6 recipes-column'>
+                                <FavoritedRecipeCard recipeId={favoriteId} onUpdateFavorite={getUser} key={favoriteId} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
