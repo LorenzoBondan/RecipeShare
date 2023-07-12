832aa34288d384d6b517902d62d37cdfb812a883
LorenzoBondan <lbbondan@gmail.com>
2023-07-12 17:32:11 -0300

    edit profile modal form

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useCallback, useEffect, useState } from 'react';
 import './styles.css';
 import { User } from 'types';
@@ -9,6 +10,8 @@ import FavoritedRecipeCard from './FavoritedRecipeCard';
 import { GiNoodles } from 'react-icons/gi';
 import { AiFillStar } from 'react-icons/ai';
 import { BsFillGearFill } from 'react-icons/bs';
+import Modal from 'react-modal';
+import { useForm } from 'react-hook-form';
 
 const Profile = () => {
 
@@ -37,6 +40,54 @@ const Profile = () => {
         getUser();
     }, [getUser]);
 
+
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal(){
+      setIsOpen(true);
+      console.log("executou")
+    }
+  
+    function closeModal(){
+      setIsOpen(false);
+    }
+  
+    const { register, handleSubmit, formState: {errors}, setValue } = useForm<User>();
+  
+    useEffect(() => {
+      if(user){
+        requestBackend({url:`/users/${user?.id}`, withCredentials:true})
+          .then((response) => {
+              const user = response.data as User;
+  
+              setValue('name', user.name);
+              setValue('imgUrl', user.imgUrl);
+              setValue('password', user.password);
+              setValue('email', user.email);
+              setValue('favoritesId', user.favoritesId);
+              setValue('feedbacksId', user.feedbacksId);
+              setValue('recipes', user.recipes);
+              setValue('roles', user.roles);
+        })  
+      }
+    }, [user, setValue]);
+  
+    const onSubmit = (formData : User) => {
+      const params : AxiosRequestConfig = {
+          method:"PUT",
+          url : `/users/${user?.id}`,
+          data: formData,
+          withCredentials: true
+      };
+  
+      requestBackend(params)
+          .then(response => {
+              console.log('success', response.data);
+              closeModal();
+              getUser();
+          })
+    };
+
     return(
         <div className='profile-container'>
             <div className='profile-user-info base-card'>
@@ -63,13 +114,45 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className='profile-user-edit'>
-                    <a href="">
+                    <a onClick={openModal}>
                         <span></span>
                         <span></span>
                         <span></span>
                         <span></span>
                         <h6><BsFillGearFill style={{marginRight:"3px"}}/>Edit Profile</h6>
                     </a>
+                    <Modal 
+                        isOpen={modalIsOpen}
+                        onRequestClose={closeModal}
+                        contentLabel="Example Modal"
+                        overlayClassName="modal-overlay"
+                        className="modal-content"
+                        >
+                        <form onSubmit={handleSubmit(onSubmit)} className="recipe-feedback-form">
+                            <h4>Edit Profile</h4>
+                            <div className='recipe-feedback-input-container'>
+                                <label htmlFor="">Img Url</label>
+                                <input 
+                                    {...register("imgUrl", {
+                                        required: 'Campo obrigatório',
+                                        pattern: { 
+                                        value: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm,
+                                        message: 'Insira uma URL válida'
+                                        }
+                                    })}
+                                    type="text"
+                                    className={`form-control base-input ${errors.imgUrl ? 'is-invalid' : ''}`}
+                                    placeholder="URL of user's image"
+                                    name="imgUrl"
+                                />
+                                <div className='invalid-feedback d-block'>{errors.imgUrl?.message}</div>
+                            </div>
+                            <div className="recipe-feedback-buttons">
+                                <button onClick={closeModal} className="btn">Close</button>
+                                <button className="btn">Submit</button>
+                            </div>
+                        </form>
+                    </Modal>
                 </div>
             </div>
             <div className='profile-user-recipes'>
